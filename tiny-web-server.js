c58cf8504745d89445583f8ed9f50fad7ca12f69
Martin Wintz <martin.wintz@gmail.com>
2014-01-25 11:08:25 -0500

    Added tiny server for local development.

---
+++ tiny-web-server.js
@@ -0,0 +1,42 @@
+["fs", "http", "mime", "path", "url" ].forEach(function (module) {
+  global[module] = require(module);
+});
+
+var config = {
+  'port': 8000,
+  'dir': 'site'
+};
+
+var server = http.createServer(function (req, res) {
+
+  var pathname = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), config.dir, pathname);
+
+  if (!path.extname(filename)) { filename = filename + '/index.html'; }
+
+  path.exists(filename, function (gotPath) {
+
+    if (!gotPath) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('404 Not Found');
+        res.end();
+        return;
+    }
+
+    res.writeHead(200, { 'Content-Type': mime.lookup(filename) });
+
+    fs
+      .createReadStream(filename, {
+        flags: 'r',
+        encoding: 'binary',
+        mode: 0666,
+        bufferSize: 4 * 1024
+      })
+      .addListener('data', function (chunk) { res.write(chunk, 'binary'); })
+      .addListener('close', function () { res.end(); });
+
+  });
+});
+
+server.listen(config.port);
+
