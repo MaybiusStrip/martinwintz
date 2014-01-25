ce32f0f72ae3094e86f1c6766fc0b9b5a9aa8f18
Martin Wintz <martin.wintz@gmail.com>
2014-01-25 11:48:33 -0500

    Added copy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,27 @@
 module.exports = function (grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
 
     nodemon: {
       dev: {
         script: 'server.js'
       }
+    },
+
+    copy: {
+      all: {
+        files: [
+          { src: 'site/index.html', dest: '_site/' },
+          { src: 'site/images/', dest: '_site/images/' },
+        ]
+      }
     }
 
   });
 
-  grunt.loadNpmTasks('grunt-nodemon');
+
 
 };
 
